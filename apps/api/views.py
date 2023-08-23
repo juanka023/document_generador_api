adf18de54849762a973d84550559274a4f4ceb07
Juan C. Sarmiento <jsarmientorosso@gmail.com>
2023-08-23 14:24:13 -0500

    terminated support for .txt file input - error reading .docx file

--- apps/api/views.py
+++ apps/api/views.py
@@ -8,41 +8,61 @@ from rest_framework.views import APIView
 class DocumentGenerationView(APIView):
     def post(self, request):
         data = request.data
-        nombre = data.get('nombre', '')
-        placa = data.get('placa', '')
-        entidad = data.get('entidad', '')
-
-        context = Context({
-            "nombre": nombre,
-            "placa": placa,
-            "entidad": entidad
-        })
-
-        template = "Se remite a Sr(a) {{nombre}} la disposición de presentarse en la entidad {{entidad}} con su vehículo de placa {{placa}}."
-        template = Template(template)
-        generated_text = template.render(context)
+        input_file = data.get('input_file', None)
+        txt_template = "Se remite a Sr(a) {{nombre}} la disposición de presentarse en la entidad {{entidad}} con su vehículo de placa {{placa}}."
 
 
+        if input_file:
+            # Read content of the uploaded file
+            content = input_file.read().decode('utf-8')
+
+            if input_file.content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                # If it's a DOCX, generate text from template
+                txt_template = Template(txt_template)
+                context = {}
+                for line in content.splitlines():
+                    key, value = line.split(':', 1)
+                    context[key.strip()] = value.strip()
+
+            else:
+                # Assuming plain text input
+                context = {}
+                for line in content.splitlines():
+                    key, value = line.split(':', 1)
+                    context[key.strip()] = value.strip()
+                txt_template = Template(txt_template)
+
+        else:
+            # Use data directly from JSON
+            context = {
+                "nombre": data.get('nombre', ''),
+                "placa": data.get('placa', ''),
+                "entidad": data.get('entidad', ''),
+            }
+
+            # Generate .txt file
+            txt_template = Template(txt_template)
+
+        # Generate .txt file
+        generated_text = txt_template.render(Context(context))
         txt_file = io.BytesIO()
         txt_file.write(generated_text.encode('utf-8'))
         txt_file.seek(0)
 
         # Generate .docx file
-        document = Document()
-        document.add_paragraph(generated_text)
+        docx_template = Document()
+        docx_template.add_paragraph(generated_text)
         docx_file = io.BytesIO()
-        document.save(docx_file)
+        docx_template.save(docx_file)
         docx_file.seek(0)
 
-
+        # Create a ZIP file containing both files
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-            txt_file_name = 'GeneratedFile-{0}.txt'.format(placa)
-            docx_file_name = 'GeneratedFile-{0}.docx'.format(placa)
-            zip_file.writestr(txt_file_name, txt_file.read())
-            zip_file.writestr(docx_file_name, docx_file.read())
+            zip_file.writestr('GeneratedFile.txt', txt_file.getvalue())
+            zip_file.writestr('GeneratedFile.docx', docx_file.getvalue())
 
-
+        # Prepare response for the ZIP file
         response = HttpResponse(zip_buffer.getvalue(), content_type='application/zip')
         response['Content-Disposition'] = 'attachment;filename=GeneratedFiles.zip'
 
