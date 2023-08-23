1e754efffb41d53fe264afdae2d11866415175be
Juan C. Sarmiento <jsarmientorosso@gmail.com>
2023-08-23 14:46:19 -0500

    technical test completed

--- apps/api/views.py
+++ apps/api/views.py
@@ -6,63 +6,68 @@ from django.template import Context, Template
 from rest_framework.views import APIView
 
 class DocumentGenerationView(APIView):
+
+    def generate_text_from_template(self, context):
+        txt_template = "Se remite a Sr(a) {{nombre}} la disposición de presentarse en la entidad {{entidad}} con su vehículo de placa {{placa}}."          
+        txt_template = Template(txt_template)
+        generated_text = txt_template.render(Context(context))
+
+        return generated_text
+
+
     def post(self, request):
         data = request.data
         input_file = data.get('input_file', None)
-        txt_template = "Se remite a Sr(a) {{nombre}} la disposición de presentarse en la entidad {{entidad}} con su vehículo de placa {{placa}}."
-
+        
+        generated_text = ""        
 
         if input_file:
-            # Read content of the uploaded file
-            content = input_file.read().decode('utf-8')
-
             if input_file.content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                # If it's a DOCX, generate text from template
-                txt_template = Template(txt_template)
-                context = {}
-                for line in content.splitlines():
-                    key, value = line.split(':', 1)
-                    context[key.strip()] = value.strip()
+                docx_template = Document(io.BytesIO(input_file.read()))
+                cont_text = ""
 
+                for paragraph in docx_template.paragraphs:
+                    cont_text += paragraph.text + "\n"
+
+                context = {}
+                for line in cont_text.splitlines():
+                    parts = line.split(':', 1)
+                    if len(parts) == 2:
+                        key, value = parts
+                        context[key.strip()] = value.strip()
+                
+                generated_text = self.generate_text_from_template(context)
             else:
-                # Assuming plain text input
+                content = input_file.read().decode('utf-8')
                 context = {}
                 for line in content.splitlines():
                     key, value = line.split(':', 1)
                     context[key.strip()] = value.strip()
-                txt_template = Template(txt_template)
-
+                generated_text = self.generate_text_from_template(context)
         else:
-            # Use data directly from JSON
             context = {
                 "nombre": data.get('nombre', ''),
                 "placa": data.get('placa', ''),
                 "entidad": data.get('entidad', ''),
             }
 
-            # Generate .txt file
-            txt_template = Template(txt_template)
+            generated_text = self.generate_text_from_template(context)
 
-        # Generate .txt file
-        generated_text = txt_template.render(Context(context))
         txt_file = io.BytesIO()
         txt_file.write(generated_text.encode('utf-8'))
         txt_file.seek(0)
 
-        # Generate .docx file
         docx_template = Document()
         docx_template.add_paragraph(generated_text)
         docx_file = io.BytesIO()
         docx_template.save(docx_file)
         docx_file.seek(0)
 
-        # Create a ZIP file containing both files
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
             zip_file.writestr('GeneratedFile.txt', txt_file.getvalue())
             zip_file.writestr('GeneratedFile.docx', docx_file.getvalue())
 
-        # Prepare response for the ZIP file
         response = HttpResponse(zip_buffer.getvalue(), content_type='application/zip')
         response['Content-Disposition'] = 'attachment;filename=GeneratedFiles.zip'
 
